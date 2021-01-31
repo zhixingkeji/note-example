f98fee43181100bbd80ea2297b7f4de270003ff6
wangle <1973551473@qq.com>
2021-02-01 01:45:33 +0800

    Update main.js

--- 购物车模块/main.js
+++ 购物车模块/main.js
@@ -51,11 +51,9 @@ const app = new Vue({
 
 	computed:{
 		totalPrice(){
-			let totalPrice = 0;
-			for(let i =0; i<this.books.length; i++){
-				totalPrice += this.books[i].price * this.books[i].count
-			}
-			return totalPrice;
+			return this.books.reduce(function (pre,book){
+				return pre + book.price * book.count
+			},0)
 		}
 
 	},
